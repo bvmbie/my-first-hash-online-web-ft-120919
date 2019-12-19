f9903ccf2fe0eff9569b20897e5a810e39126c38
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 20:47:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,11 +32,12 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest
+  shipping_manifest["muskets"]=2
+  shipping_manifest["gun powder"]=4
 
 
 
-  # add 4 gun powder to the shipping_manifest hash below
+  
 
 
 
