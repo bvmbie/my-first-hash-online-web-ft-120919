e7b3eae829f27d538522bf2883404f4766670eb7
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 20:38:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,9 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash = {
+    "milk" => 1
+    "bread" => 2
+    "cheese" => 3
+  }
 
   
 end
